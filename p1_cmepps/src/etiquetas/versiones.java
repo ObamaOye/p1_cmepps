f8f8b29bf5dc17b9926255c7f3377248880bd192
ObamaOye <m.obamaoye3006@gmail.com>
2022-11-14 11:00:46 +0100

    Subiendo branch 2 - tag2

--- p1_cmepps/src/etiquetas/versiones.java
+++ p1_cmepps/src/etiquetas/versiones.java
@@ -5,6 +5,7 @@ public class versiones {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Bienvenidos al control de versiones en github - Tag1");
+		System.out.println("Hemos creado y subido la rama tag2");
 		
 	}
 
