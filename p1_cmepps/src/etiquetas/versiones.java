703e1ce64baf9b1846638baaba64c5aef3564abe
ObamaOye <m.obamaoye3006@gmail.com>
2022-11-14 11:44:13 +0100

    Algunos cambios en tag3

--- p1_cmepps/src/etiquetas/versiones.java
+++ p1_cmepps/src/etiquetas/versiones.java
@@ -4,8 +4,8 @@ public class versiones {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Bienvenidos al control de versiones en github - Tag1");
-		System.out.println("Hemos creado y subido la rama tag2");
+		//System.out.println("Bienvenidos al control de versiones en github - Tag1");
+		//System.out.println("Hemos creado y subido la rama tag2");
 		System.out.println("Ya estamos en la rama tag3");
 		
 	}
