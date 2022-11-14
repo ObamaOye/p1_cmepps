3693dedb33392fcd2ab4629656e99faceb59601b
ObamaOye <m.obamaoye3006@gmail.com>
2022-11-14 10:46:59 +0100

    Estado inicial - creando tag1

---
+++ p1_cmepps/src/etiquetas/versiones.java
@@ -0,0 +1,11 @@
+package etiquetas;
+
+public class versiones {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Bienvenidos al control de versiones en github - Tag1");
+		
+	}
+
+}
